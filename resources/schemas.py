de2c69bf0200b57dfbc11f47d0887738d18c1e24
qazw7910 <okmijn7980@gmail.com>
2023-04-27 23:52:35 +0800

    Give the schmas denotation

--- resources/schemas.py
+++ resources/schemas.py
@@ -2,9 +2,13 @@ from marshmallow import Schema, fields
 
 
 class ItemSchema(Schema):
-    # dump_only is only included in the serialized dictionary when the object is initially created
+    # dump_only=True is only included in the serialized dictionary when the object is initially created
+    # dump_only in my understanding often used in item create
+    #----------------------------------------------------------------
     # required=True parameter is used to specify that a field is mandatory during deserialization,
     # meaning it must be present when converting a dictionary or JSON object to an object of a certain type.
+    # required=True in my understanding it must be present when converting a dictionary or JSON object to an
+    # object of a certain type.
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
@@ -18,5 +22,6 @@ class ItemUpdateSchema(Schema):
 
 class StoreSchema(Schema):
     # dump_only is only included in the serialized dictionary when the object is initially created
+    # dump_only often used in item create
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
