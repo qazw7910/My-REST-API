dbe8d9e26d623e2ad09287654d7ffb442e548715
qazw7910 <okmijn7980@gmail.com>
2023-04-29 13:46:18 +0800

    one-to-many relationships using SQLAlchemy

--- models/item.py
+++ models/item.py
@@ -2,10 +2,13 @@ from db import db
 
 
 class ItemModel(db.Model):
-    __tablename__ = 'items'
+    __tablename__ = "items"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False, nullable=False)
-    price = db.Column(db.Float(precision=2), unique=False, ullable=False)
-    store_id = db.Column(db.Integer, db.ForeignKey("stores.id"), unique=False, nullable=False)
-    store = db.relationship("StoreModel", backpopulates="items")
+    price = db.Column(db.Float(precision=2), unique=False, nullable=False)
+
+    store_id = db.Column(
+        db.Integer, db.ForeignKey("stores.id"), unique=False, nullable=False
+    )
+    store = db.relationship("StoreModel", back_populates="items")
--- models/store.py
+++ models/store.py
@@ -1,12 +1,12 @@
+
 from db import db
 
 
-
 class StoreModel(db.Model):
-    __tablename__ = 'stores '
+    __tablename__ = "stores"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     # define in schema.py used Nested
     # items = fields.List(fields.Nested(PlainStoreSchema(), dump_only=True))
-    items = db.relationship("ItemModel", backpopulate="store", lazy="dynamic")
+    items = db.relationship("ItemModel", back_populates="store", lazy="dynamic")
