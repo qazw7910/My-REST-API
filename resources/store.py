7a3970b041984a40ffcd38855cf2f8940acb7bf4
qazw7910 <okmijn7980@gmail.com>
2023-05-01 06:50:52 +0800

    Add delete func mod to method of sqlalchemy

--- resources/store.py
+++ resources/store.py
@@ -7,8 +7,6 @@ from db import db
 from models import StoreModel
 from resources.schemas import StoreSchema
 
-
-
 blp = Blueprint("Stores", __name__, description="Operations on stores")
 
 
@@ -17,17 +15,20 @@ class Store(MethodView):
     @blp.response(200, StoreSchema)
     def get(self, store_id):
         store = StoreModel.query.get_or_404(store_id)
+        return store
 
     def delete(self, store_id):
         store = StoreModel.query.get_or_404(store_id)
-        raise NotImplementedError("Deleting an Item is not implemented.")
+        db.session.delete(store)
+        db.session.commit()
+        return {"message": "Store deleted."}, 200
 
 
 @blp.route("/store")
 class StoreList(MethodView):
     @blp.response(200, StoreSchema(many=True))
     def get(self):
-        return stores.values()
+        return StoreModel.query.all()
 
     @blp.arguments(StoreSchema)
     @blp.response(200, StoreSchema)
